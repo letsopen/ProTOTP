41e6f6ae5a677721be5f5a550b8231e6c0fd62df
Administrator <administrator@localhost.localdomain>
2026-01-16 11:26:07 +0800

    优化展示

--- ProTOTP/Converters/StringToColorConverter.cs
+++ ProTOTP/Converters/StringToColorConverter.cs
@@ -11,25 +11,53 @@ namespace ProTOTP.Converters
         {
             if (value is string colorName)
             {
+                // 简单判断目标类型，避免复杂的反射操作
+                var color = Colors.Gray; // 默认颜色
+                
                 switch (colorName.ToLower())
                 {
                     case "green":
-                        return Colors.Green;
+                        color = Colors.Green;
+                        break;
                     case "red":
-                        return Colors.Red;
+                        color = Colors.Red;
+                        break;
                     case "orange":
-                        return Colors.Orange;
+                        color = Colors.Orange;
+                        break;
                     case "blue":
-                        return Colors.Blue;
+                        color = Colors.Blue;
+                        break;
                     case "yellow":
-                        return Colors.Yellow;
+                        color = Colors.Yellow;
+                        break;
                     case "purple":
-                        return Colors.Purple;
+                        color = Colors.Purple;
+                        break;
                     default:
-                        return Colors.Gray;
+                        color = Colors.Gray;
+                        break;
+                }
+                
+                // 对于ProgressBar（SolidColorBrush.Color），返回Color
+                // 对于TextBlock（TextBlock.Foreground），返回Brush
+                // 我们可以通过参数来区分，或者简单地总是返回Brush
+                // 因为SolidColorBrush可以接受Color作为参数，但TextBlock.Foreground需要Brush
+                // 为了兼容两者，我们可以检查参数
+                
+                // 如果参数包含特定标识，表示需要Color而非Brush
+                if (parameter != null && parameter.ToString() == "Color")
+                {
+                    return color;
+                }
+                else
+                {
+                    // 默认返回Brush，适用于TextBlock.Foreground
+                    return new SolidColorBrush(color);
                 }
             }
-            return Colors.Gray;
+            // 默认返回Brush
+            return new SolidColorBrush(Colors.Gray);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
